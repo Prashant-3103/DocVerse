e6580c3b157f5127949ca8c0e741c1cd48a783e1
Prashant-3103 <prashantkumarchoudhary.211mn035@nitk.edu.in>
2025-01-18 00:05:02 +0530

    Refactor upload and S3 services to remove commented-out code and improve readability

--- src/s3services.js
+++ src/s3services.js
@@ -1,21 +1,4 @@
-// import AWS from "aws-sdk";
-// import fs from "fs";
 
-// const s3 = new AWS.S3({
-// 	accessKeyId: process.env.MY_AWS_ACCESS_ID,
-// 	secretAccessKey: process.env.MY_AWS_ACCESS_KEY,
-// 	region: process.env.MY_AWS_REGION,
-// });
-
-// export const s3Upload = async (bucket, file) => {
-// 	const params = {
-// 		Bucket: bucket,
-// 		Key: file.name,
-// 		Body: fs.createReadStream(file.path),
-// 	};
-
-// 	return await s3.upload(params).promise()
-// }
 import AWS from "aws-sdk";
 import fs from "fs";
 
